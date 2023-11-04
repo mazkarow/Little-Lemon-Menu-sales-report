6e3f72149d6c3c823ca7c01fdce4cfabfb8eb45c
Murtaza Kanorwala <makanor@gmail.com>
2023-11-04 07:10:56 -0400

    C

--- stored_procedures.sql
+++ stored_procedures.sql
@@ -1,10 +1,16 @@
--- stored_procedures.sql
-
-DELIMITER $$
-
+-- Create `GetEmployeeList` Stored Procedure
 CREATE PROCEDURE `GetEmployeeList`()
 BEGIN
     SELECT * FROM employees;
-END$$
+END;
 
-DELIMITER ;
+-- Create `TotalSalesByEmployee` Stored Procedure
+CREATE PROCEDURE `TotalSalesByEmployee`()
+BEGIN
+    SELECT e.name, SUM(p.price * s.quantity) AS total_sales
+    FROM sales s
+    JOIN employees e ON s.employee_id = e.employee_id
+    JOIN products p ON s.product_id = p.product_id
+    GROUP BY e.employee_id;
+END;
+
